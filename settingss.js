6d4f3ca96bf10571878f005599c858f8b1afbd3f
besthama10 <hamandishebest2@gmail.com>
2025-09-10 20:00:17 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~83Uh0YIS#REvTurBBoCIqHm-hvIck6HrjtVrePrjkTfJYRONioFI",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "263789742034", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
